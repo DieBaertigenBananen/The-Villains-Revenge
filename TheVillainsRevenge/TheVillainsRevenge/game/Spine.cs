8bb3f9ae9a833fe6f311793e3a6af3a69b273ea5
sensorreinigung <jens@richter-fam.de>
2014-02-10 13:32:18 +0100

    Da haste deinen Sack
    und die Prinzessin.

--- TheVillainsRevenge/TheVillainsRevenge/game/Spine.cs
+++ TheVillainsRevenge/TheVillainsRevenge/game/Spine.cs
@@ -39,7 +39,7 @@ namespace TheVillainsRevenge
             {
                 animationState.SetAnimation(1, newanim, loop);
             }
-            else if (newanim == "sc_escape" || newanim == "sc_cover_eyes" || newanim == "sc_bag" || newanim == "sc_dummy")
+            else if (newanim == "sc_escape" || newanim == "sc_cover_eyes" || newanim == "sc_bag")
             {
                 animationState.SetAnimation(2, newanim, loop);
             }
